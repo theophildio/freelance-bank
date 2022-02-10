45c4ae9743590aedf8e803dfcd0de56d93f01e5c
theophildio <dtheo.sangma@gmail.com>
2022-02-10 22:59:19 +0600

    Current balance calculation updated

--- js/banking.js
+++ js/banking.js
@@ -1,11 +1,21 @@
+// Deposit caculation
 document.getElementById('deposit-button').addEventListener('click', function() {
   const depositField = document.getElementById('deposit-input');
   const depositAmount = parseFloat(depositField.value);
   const depositTotal = document.getElementById('deposit-total');
   const currentDepositBalance = depositTotal.innerText;
-  const convertCurrentDepositBalance = parseFloat(currentDepositBalance);
-  const updateDepositBalance = convertCurrentDepositBalance + depositAmount;
+  const newCurrentDepositBalance = parseFloat(currentDepositBalance);
+  const updateDepositBalance = newCurrentDepositBalance + depositAmount;
   depositTotal.innerText = updateDepositBalance;
   // clear deposit field
   depositField.value = ''; 
+  // Add deposit to current balance
+  const mainBalance = document.getElementById('current-amount');
+  const mainBalanceTotal = mainBalance.innerText;
+  const newMainBalanceTotal = parseFloat(mainBalanceTotal);
+  const updateNewMainBalance = newMainBalanceTotal + depositAmount;
+  mainBalance.innerText = updateNewMainBalance;
 });
+
+// Withdraw calculation
+
