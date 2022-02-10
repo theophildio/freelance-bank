d00705c188b4598744903731d79e355c0866159f
theophildio <dtheo.sangma@gmail.com>
2022-02-10 21:04:42 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,16 @@
+document.getElementById('login-button').addEventListener('click', function() {
+  // get user email
+  const inputEmail = document.getElementById('user-email');
+  const userEmail = inputEmail.value;
+  // get user password
+  const inputPassword = document.getElementById('user-password');
+  const userPassword = inputPassword.value;
+  if (userEmail == 'info@login.com' && userPassword == '12345678') {
+    window.location.href = 'bank.html';
+  } else {
+    console.log("Email and password doesn't match!");
+  }
+  // clear user input fields
+  inputEmail.value = '';
+  inputPassword.value = '';
+})
